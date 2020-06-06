0acb3dd59bd2d13c6d4762dd292cc3af7d717212
Aman Tiwari <tiwari.aman85@gmail.com>
2020-06-06 23:14:20 +0530

    [Fix] Filtered dashboard page entries

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,13 @@ import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { DataService } from '../data.service';
 
+// export interface DashboardAPI {
+//   Hostname: number,
+//   IdentityFile: string,
+//   Port: string,
+//   User: string
+// }
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -14,17 +21,30 @@ export class DashboardComponent {
   public hosts = null;
   regularDistribution = 100 / 3.2 + '%';
 
-
-  constructor(
-    private dataService: DataService
-  ) {}
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
-
     this.dataService
       .getData('http://localhost:5000/ssh/host')
       .subscribe((data: any[]) => {
-        this.hosts = data["data"];
+        this.hosts = this.filter_dashboard_keys(data['data']);
       });
+  }
+
+  filter_dashboard_keys(data: []) {
+    const allowed = ['Hostname', 'IdentityFile', 'Port', 'User'];
+    var response = {};
+    for (let key in data) {
+      let value = data[key];
+
+      const filtered = Object.keys(value)
+        .filter((key) => allowed.includes(key))
+        .reduce((obj, key) => {
+          obj[key] = value[key];
+          return obj;
+        }, {});
+      response[key] = filtered;
+    }
+    return response;
   }
 }
